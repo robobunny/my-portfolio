8e691b186aa601bedf9a602b4c8a83ee7ba4c7a3
William Duraney <william.duraney@gmail.com>
2020-10-04 00:37:38 -0400

    tweak nav button animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,11 @@
-import { Link } from 'gatsby'
 import React from "react"
 import { Navigation } from './Navigation'
 import style from "./header.module.scss"
 
-const Header = (props) => (
+const Header = () => (
   <header className={style.header}>
-    <p className={style.image}>bunny</p>
-    <h2 className={style.siteTitle}>
-      <Link to="/">{props.title}</Link>
-    </h2>
-    <span className={style.siteSubtitle}>
-      {props.subtitle}
-    </span>
     <Navigation />
   </header>
 )
-
-Header.defaultProps = {
-  title: "William C. Duraney",
-  subtitle: "Fullstack Javascript Developer"
-}
 
 export default Header
