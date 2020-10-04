eaaac9ad50f99d550e87a724b880a7a57545de1e
William Duraney <william.duraney@gmail.com>
2020-10-04 01:09:23 -0400

    add image to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,63 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import style from "./home.module.scss"
 import {Navigation} from "../components/Header/Navigation"
+import Img from "gatsby-image"
 
-const IndexPage = (props) => {
+const IndexPage = ({ data }, ...props) => {
+  const imgSrc = [
+    data.mobileBunny.childImageSharp.fluid,
+    {
+      ...data.desktopBunny.childImageSharp.fluid,
+      media: `(min-width: 768px)`,
+    },
+    {
+      ...data.hiDpiBunny.childImageSharp.fluid,
+      media: `(min-width: 1440px)`,
+    },
+  ]
+
   return (
     <div className={style.splashPage}>
       <SEO title="Home" />
-      <p className={style.image}>bunny</p>
-      <h2 className={style.siteTitle}>
-        <Link to="/">{props.title}</Link>
-      </h2>
-      <span className={style.siteSubtitle}>{props.subtitle}</span>
+      <Img
+        fluid={imgSrc}
+        className={style.image}
+      />
+      <div className={style.info}>
+        <h2 className={style.siteTitle}>William C. Duraney</h2>
+        <p className={style.siteSubtitle}>Fullstack Javascript Developer</p>
+      </div>
       <Navigation />
     </div>
     )
 }
 
-IndexPage.defaultProps = {
-  title: "William C. Duraney",
-  subtitle: "Fullstack Javascript Developer",
-}
+export const query = graphql`
+  {
+    mobileBunny: file(relativePath: { eq: "bunny256.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 256) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    desktopBunny: file(relativePath: { eq: "bunny1024.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 512) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    hiDpiBunny: file(relativePath: { eq: "bunny1024.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1024) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
