3b89b6b7b63fe3ca4003db6aee09b69bd26b1439
William Duraney <william.duraney@gmail.com>
2020-10-06 14:24:33 -0400

    add profile photo and resume link to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,9 @@
-import { graphql } from "gatsby"
+import { graphql, navigate } from "gatsby"
 import React from "react"
 import SEO from "../components/seo"
 import HomeLayout from "../layouts/HomeLayout"
 import style from "./about.module.scss"
+import Img from "gatsby-image"
 
 const AboutPage = ({ data }) => {
   const infoText = data.aboutInfo.childMarkdownRemark.html;
@@ -10,7 +11,9 @@ const AboutPage = ({ data }) => {
     <HomeLayout>
       <SEO title="About William Duraney" />
       <h1 className={style.title}>About Me</h1>
-      <div className={style.content} dangerouslySetInnerHTML={{ __html: infoText }}/>
+      <button onClick={(()=>{navigate("resume")})} className={style.button}>My Resume</button>
+        <Img fluid={data.profilePic.childImageSharp.fluid} alt="William C. Duraney in a park" className={style.profilePic}/>
+        <div className={style.content} dangerouslySetInnerHTML={{ __html: infoText }}/>
     </HomeLayout>
   )
 }
@@ -20,6 +23,13 @@ export const query = graphql`
     aboutInfo: file(relativePath: {eq: "about/aboutMe.md"}) {
       childMarkdownRemark {
         html
+      }
+    }
+    profilePic: file(relativePath: {eq: "profile-pic.png"}) {
+      childImageSharp {
+        fluid (maxWidth: 512) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
       }
     }
   }
