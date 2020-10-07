5c0d189d1b55b1b123b75f745dfcbb93ac86cf70
William Duraney <william.duraney@gmail.com>
2020-10-07 12:32:17 -0400

    update contact page and style for mobile

--- src/copy/contact/contactInfo.md
+++ src/copy/contact/contactInfo.md
@@ -1,3 +1,1 @@
-This is the text for the Contact section. It comes from the .md file in the `/copy/contact/` directory. Soon, I'll make a form so you can actually contact me. Probably pretty important on a portfolio site tbh.
-
-Any questions?
+Feel free to get in touch with me:
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,39 @@ import { graphql } from "gatsby"
 import React from "react"
 import SEO from "../components/seo"
 import HomeLayout from "../layouts/HomeLayout"
+import style from "./contact.module.scss"
 
 const ContactPage = ({ data }) => {
   const infoText = data.contactInfo.childMarkdownRemark.html;
   return (
     <HomeLayout>
       <SEO title="Contact" />
-      <h1>Contact Me</h1>
+      <h1 className={style.title}>Contact Me</h1>
       <div className="article-text" dangerouslySetInnerHTML={{ __html: infoText }} />
+      <p>
+        <a 
+          href="mailto:williamduraney@gmail.com" 
+          className={style.button}
+        >
+          Email
+        </a>
+      </p>
+      <p>
+        <a 
+          href="https://github.com/robobunny" 
+          className={style.button}
+        >
+          Github
+        </a>
+      </p>
+      <p>
+        <a 
+          href="https://www.linkedin.com/in/william-duraney" 
+          className={style.button}
+        >
+          LinkedIn
+        </a>
+      </p>
     </HomeLayout>
   )
 }
